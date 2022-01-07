674fc35cdd4b28ad55857deedf198ccbd772f4b4
Asnur Ramdani <asnurramdhani12@gmail.com>
2022-01-08 00:00:40 +0700

    fix oop

--- API.py
+++ API.py
@@ -4,40 +4,38 @@ from os import system
 
 class Provinsi:
     total_data = 10
+    url = "https://dev.farizdotid.com/api/daerahindonesia/provinsi"
+    data = []
 
-    def __init__(self, order="", page=1):
-        self.order = order.upper()
-        self.page = page
-        self.url = "https://dev.farizdotid.com/api/daerahindonesia/provinsi"
-        self.data = []
-
-    def dataProvinsi(self):
+    def dataProvinsi(self, order = "ASC", page=1):
         header = ['No', 'Nama Provinsi']
         response = requests.get(self.url).json()['provinsi']
         for i in range(len(response)):
             self.data.append([i+1, response[i]['nama']])
-        start = self.total_data * (self.page -1)
+        start = self.total_data * (page -1)
         self.data = self.data[start:start+self.total_data]
-        self.data = sorted(self.data, reverse=True if self.order == "DESC" else False)
+        self.data = sorted(self.data, reverse=True if order == "DESC" else False)
         print(tabulate(self.data, header, tablefmt="github"))
 
+    def preview(self):
+        system('cls')
+        print("="*16)
+        print("DATA PROVINSI")
+        print("="*16)
+        print()
 
-def preview():
-    system('cls')
-    print("="*16)
-    print("DATA PROVINSI")
-    print("="*16)
-    print()
+        order = input("Urutan Data ASC/DESC : ")
+        page = int(input("Halaman : "))
+        # data = Provinsi(order, page)
 
-    order = input("Urutan Data ASC/DESC : ")
-    page = int(input("Halaman : "))
-    data = Provinsi(order, page)
+        self.dataProvinsi(order, page)
 
-    data.dataProvinsi()
+        again = input("Lihat Data Lainnya (Y/N) : ").upper()
 
-    again = input("Lihat Data Lainnya (Y/N) : ").upper()
+        if again == "Y":
+            self.preview()
 
-    while again == "Y":
-        preview()
 
-preview()
+view = Provinsi()
+
+view.preview()
